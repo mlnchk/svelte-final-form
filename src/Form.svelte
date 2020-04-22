c314b26febb0b9d766aaf7784345c3c4ec127d62
mlnchk <danil.mlnchk@gmail.com>
2020-04-22 19:12:33 +0300

    add: form component

---
+++ src/Form.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { createForm } from 'final-form'
+  export let onSubmit, validate, initialValues
+
+  const form = createForm({
+    initialValues,
+    onSubmit,
+    validate,
+  })
+</script>
+
+<form {...$$restProps} on:submit|preventDefault={form.submit}>
+  <slot {form}></slot>
+</form>
